784fe1020dc864566df6a6c90a6168cf86a9ed64
merzouka <marzoukayouness@gmail.com>
2026-07-23 21:26:22 +0100

    chore(auth): Added user entity to module imports

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Admin } from './admin.entity';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
+import { User } from '../../../typeorm/entities/User';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Admin])],
+  imports: [TypeOrmModule.forFeature([Admin, User])],
   controllers: [AdminController],
   providers: [AdminService],
   exports: [AdminService],
--- src/modules/parent/parent.module.ts
+++ src/modules/parent/parent.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Parent } from './parent.entity';
 import { ParentService } from './parent.service';
+import { User } from '../../../typeorm/entities/User';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Parent])],
+  imports: [TypeOrmModule.forFeature([Parent, User])],
   providers: [ParentService],
   exports: [ParentService],
 })
