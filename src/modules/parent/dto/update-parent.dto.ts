2d9938d41620cc782a3793be225aa1a81648377b
merzouka <marzoukayouness@gmail.com>
2026-07-24 16:09:30 +0100

    fix(auth): Fixed parent update dto to be looser

--- src/modules/parent/dto/update-parent.dto.ts
+++ src/modules/parent/dto/update-parent.dto.ts
@@ -1,9 +1,14 @@
+import { IsOptional, IsString } from 'class-validator';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdateParentDto {
   @ApiPropertyOptional({ description: 'PIN hash (if re-setting)' })
+  @IsOptional()
+  @IsString()
   pin_hash?: string;
 
   @ApiPropertyOptional({ example: '+212600000001' })
+  @IsOptional()
+  @IsString()
   phone_number?: string;
 }
